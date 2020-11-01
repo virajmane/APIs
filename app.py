e6a84cb87143beeb4dfd5f251b43b570b3981354
Viraj <62030782+virajmane@users.noreply.github.com>
2020-11-01 21:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -179,5 +179,5 @@ def msg():
     return jsonify(final)
 
 if __name__ == "__main__":
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True)
